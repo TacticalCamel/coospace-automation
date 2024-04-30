7773d3552cd07af54906a6c0d05b36985b3caa12
Takács Balázs <takacsbalazs2014@gmail.com>
2024-04-30 23:22:26 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome()
+browser.get('https://www.google.com')
+
+input()
+
+browser.quit()
